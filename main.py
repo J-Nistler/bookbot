3948b5d31226991abb8fad826282da932d8eafe8
J-Nistler <jmnistl@protonmail.com>
2025-04-27 15:52:20 -0400

    complete 1.0 of character counter and print results

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from stats import count_words
+from stats import count_words, count_letters, sort_results
+
+filepath = "books/frankenstein.txt"
 
 def get_book_text (filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def print_results (word_count, sorted_results):
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {filepath}...")   
+    print ("----------- Word Count ----------")
+    print (f"Found {word_count} total words")
+    print ("--------- Character Count -------")
+    for letter in sorted_results:
+        print(f"{letter.get("char")}: {letter.get("num")}")
+    print ("============= END ===============")
+    return
+
 def main ():
-    print(count_words(get_book_text("./books/frankenstein.txt")))
+    results = sort_results(count_letters(get_book_text(filepath)))
+    wordcount = count_words(get_book_text(filepath))
+    print_results(wordcount, results)
     return
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,30 @@
 def count_words (corpus):
     word_count = 0
     word_count = len(corpus.split())
-    return f"{word_count} words found in the document"
+    return word_count
+
+def count_letters (corpus):
+    letter_list = [char.lower() for char in corpus]
+    char_dict = {}
+
+    for char in letter_list:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_results (char_dict):
+    sorted_list = []
+
+    for key in char_dict:
+        if key.isalpha():
+            new_dict = {}
+            new_dict["char"] = key
+            new_dict["num"] = char_dict.get(key)
+            sorted_list.append(new_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
